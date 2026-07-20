a87e41413785e6e9558a654d56a29fea2bb1dc74
MD. Alif Khan <office.alifkhan@gmail.com>
2026-07-20 19:26:32 +0600

    feat: implement Google OAuth authentication with user management and token handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { PassportModule } from '@nestjs/passport';
+import { GoogleAuthGuard } from './guards/google-auth.guard';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), PassportModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtAuthGuard],
+  providers: [AuthService, JwtAuthGuard, GoogleStrategy, GoogleAuthGuard],
   exports: [AuthService],
 })
 export class AuthModule {}
